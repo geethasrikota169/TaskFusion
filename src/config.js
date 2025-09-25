3439453f1136ca891fa52f89abca1f1badbc6db5
Geetha <geethasrikota169@gmail.com>
2025-09-25 19:26:26 +0530

    feat: Make backend URL configurable for deployment

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
 const config = {
-    // "url": process.env.REACT_APP_API_URL || "http://localhost:2002"
-    "url":"http://localhost:8082"
+    "url": process.env.REACT_APP_BACKEND_URL || "http://localhost:8082"
 }
 export default config;
